800afcb0136f65462516668758ff775ae103f2ff
kevindm181998 <kevin.dom.mol@gmail.com>
2022-12-19 11:58:39 -0600

    FIX BUG

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://kevindm181998.github.io/',
+  base: 'https://kevindm181998.github.io/gh-react-vite',
   plugins: [react()]
 })
